62aebee87a4a164bc27f78bd71b55ef6e345673a
Int Siemens <intSiemens@hotmail.com>
2025-01-08 15:48:14 +0200

    initial solution

---
+++ ConfirmationEmailSend/ConfirmationEmailSend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConfirmationEmailSend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
